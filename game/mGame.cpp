d46a8b423c447d59ec796fc39b0daa2eeeba3116
Mikhail Sokolovskiy <sokolmish@yandex.ru>
2020-07-12 13:27:40 +0300

    Initial commit

---
+++ game/mGame.cpp
@@ -0,0 +1,48 @@
+#include "glew.h"
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <string>
+
+#include "param.h"
+
+
+int main() {
+    // Window init
+    GLFWwindow* window;
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+    std::string title = std::string(WINDOW_TITLE) + " | v" + std::string(VERSION);
+    window = glfwCreateWindow(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, title.c_str(), NULL, NULL);
+    if (!window) {
+        std::cout << "Failed to initialize window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // GLEW init
+    glewExperimental = GL_TRUE;
+    GLenum glewStatus = glewInit();
+    if (glewStatus != GLEW_OK || !GLEW_VERSION_2_1) {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(0.509f, 0.788f, 0.902f, 1.f);
+    
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
---
+++ game/param.h
@@ -0,0 +1,10 @@
+#ifndef __PARAM_H__
+#define __PARAM_H__
+
+#define VERSION "1.0.0"
+
+#define DEFAULT_WINDOW_WIDTH 640
+#define DEFAULT_WINDOW_HEIGHT 480
+#define WINDOW_TITLE "mGame"
+
+#endif
